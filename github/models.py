e3e2ec1b6bd19a70b65fd329152b35ed9941c715
lucemia <lucemia@gmail.com>
2015-01-18 09:55:40 -0800

    update model, requirement

--- github/models.py
+++ github/models.py
@@ -1,12 +1,31 @@
 from django.db import models
+from jsonfield import JSONField
+
+
+class Repository(models.Model):
+    name = models.CharField(max_length=255)
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+
+class Event(models.Model):
+    name = models.CharField(max_length=255)
 
 # Create your models here.
-
 class EventCount(models.Model):
-    repo = models.CharField(max_length=255)
-    actor = models.CharField(max_length=255)
-    event = models.CharField(max_length=255)
+    repo = models.ForeignKey(Repository)
+    user = models.ForeignKey(User)
+    event = models.ForeignKey(Event)
     count = models.IntegerField()
 
+    class Meta:
+        unique_together = ((repo, user, event), )
 
+class Role(models.Model):
+    repo = models.ForeignKey(Repository)
+    user = models.ForeignKey(User)
 
+    contributions = JSONField(default="{}")
+
+    class Meta:
+        unique_together = ((repo, user), )
--- import_gitlog.py
+++ import_gitlog.py
@@ -7,17 +7,22 @@ def input(file):
     with open(file) as ifile:
         reader = csv.DictReader(ifile)
         for row in reader:
+            repo, _ = Repository.objects.get_or_create(name = row['repository_name'])
+            actor, _ = User.objects.get_or_create(name=row['actor'])
+            event, _ = Event.objects.get_or_create(name=row['event'])
+
             row = {
-                'repo': row['repository_name'],
-                'actor': row['actor'],
-                'event': row['type'],
+                'repo': repo,
+                'actor': actor,
+                'event': event,
                 'count': row['f0_']
             }
             try:
-                EventCount(**row).save()
+                EventCount.objects.create(**row)
             except Exception as e:
                 logging.exception(e)
 
 
 for i in os.listdir('./git-log'):
+    print 'process', i
     input('./git-log/' + i)
